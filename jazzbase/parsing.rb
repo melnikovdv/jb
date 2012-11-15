a391117332bc27dcca47a9f0418a6a03c200b2e0
dvmelnikov <melnikovdv@gmail.com>
2012-11-15 12:00:53 +0400

    parsing jazzbase file added

---
+++ jazzbase/parsing.rb
@@ -0,0 +1,30 @@
+require 'nokogiri'
+require 'open-uri'
+
+class Singer
+	def initialize(name, link)
+		@name = name
+		@link = link
+	end
+	def to_s
+		return @name + "[" + @link + "]"
+	end
+	attr_reader :name, :link
+end
+
+
+def parseSingers
+	uri = 'http://jazzbase.ru/people/765.htm'	
+	doc = Nokogiri::HTML(open(uri))	
+
+	arr = Array.new
+	doc.css('ul#ispol li a').each do |link| 
+		arr.push Singer.new(link.content, link[:href])		
+	end	
+	return arr
+end
+
+arr = parseSingers
+puts arr
+#puts arr
+puts 'Length of singers array: ' + arr.length.to_s
