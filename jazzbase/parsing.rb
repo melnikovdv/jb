0a0812de3bd97ab2add905b52f122cb083273e0c
dvmelnikov <melnikovdv@gmail.com>
2012-11-16 17:50:35 +0400

    added some attributes of singer

--- jazzbase/parsing.rb
+++ jazzbase/parsing.rb
@@ -10,6 +10,7 @@ class Singer
         return @name + " [" + @link + "]"
     end
     attr_reader :name, :link
+    attr_accessor :descr, :imgTag
 end
 
 
@@ -25,27 +26,26 @@ def parseSingers
     return arr
 end
 
-def parseSingerDescription(uri)
-  doc = Nokogiri::HTML(open(uri))   
-  doc.css('td.dbrd table').each do |el|    
-    s = el.children[2].children[0].content
-    puts s.class
-    f = File.new("settings.txt", "w")
-    f.write s
-    f.close
-    
-    puts s.empty?
-    #puts el.content
-    #exit
-  end
-  #puts doc.class
-  #puts doc.xpath('./html/body/table/tbody/tr[3]/td[2]/table/tbody/tr[3]/td/pre')
-end
+def parseSingerDescription(singer)
+  doc = Nokogiri::HTML(open(singer.link))     
+  singer.descr = doc.xpath('//html/body/table/tr[3]/td[2]/table/tr[3]/td/pre')[0].content     
+  singer.imgTag = doc.xpath('//html/body/table/tr[3]/td[2]/table/tr[2]/td/table/td/img')[0].to_s.gsub('../', 'http://jazzbase.ru/')
 
+  puts doc.xpath('/html/body/table/tr[3]/td[2]/table/tr[2]/td/table/tr/td[2]/ul[1]//li')[0].content
+  #               /html/body/table/tr[3]/td[2]/table/tr[2]/td/table/tr/td[2]/ul[1]
+  doc.xpath('/html/body/table/tr[3]/td[2]/table/tr[2]/td/table/tr/td[2]/ul[1]//li').each do |el|
+  	puts el.class
+  end
+
+#tr[3]/td[2]/table/tr[2]/td/table/tr/td[1]/img
+  # f = File.new("settings.txt", "w")
+  # f.write s
+  # f.close
+end
 arr = parseSingers
 puts 'Length of singers array: ' + arr.length.to_s
-
 #puts arr[0]
 puts arr[0].link
-parseSingerDescription arr[0].link
-
+parseSingerDescription(arr[0])
+# puts arr[0].descr
+# puts arr[0].imgTag
