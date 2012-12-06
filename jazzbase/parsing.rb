44bc27f03e48778aefb55843b38d4069e8c48ae2
mdv <melnikovdv@gmail.com>
2012-12-07 01:25:37 +0400

    parsing errors with null nodes

--- jazzbase/parsing.rb
+++ jazzbase/parsing.rb
@@ -6,7 +6,9 @@ require 'open-uri'
   todo:        
     # download singer image
     # download album image
-  
+    # make increment site parsing to xml file
+    # make create xml nodes from objects    
+    # save id from jazzbase and meka own ids
   done:
     # refactor loading of Singer (throught link in constructor)
     # output to XML
@@ -22,8 +24,16 @@ class Singer
       xPath = '/html/body/table/tr[3]/td[2]/table'
 
       @name = doc.xpath(xPath + '/tr[1]/td/h3')[0].content      
-      @descr = doc.xpath(xPath + '/tr[3]/td/pre')[0].content     
-      @img = doc.xpath(xPath + '/tr[2]/td/table/td/img')[0][:src].to_s.subLink      
+      
+      descrNode = doc.xpath(xPath + '/tr[3]/td/pre')[0]
+      if descrNode
+        @descr = descrNode.content     
+      end
+      
+      imgNode = doc.xpath(xPath + '/tr[2]/td/table/td/img')[0]
+      if imgNode
+        @img = imgNode[:src].to_s.subLink      
+      end
       @albums = Array.new
       doc.xpath(xPath + '/tr[2]/td/table/td[2]/ul[1]/li/a').each do |el|
         @albums.push(Album.new(el[:href].subLink))
@@ -49,8 +59,15 @@ class Album
     xPath = '/html/body/table/tr[3]/td[2]/table'
 
     @name = doc.xpath(xPath + '/tr[1]/td/h3')[0].content
-    @year = doc.xpath(xPath + '/tr[2]/td/table/td[2]/ul[2]/li/a')[0].content    
-    @img = doc.xpath(xPath + '/tr[2]/td/table/td[1]/img')[0][:src].subLink
+    yearNode = doc.xpath(xPath + '/tr[2]/td/table/td[2]/ul[2]/li/a')[0]
+    if yearNode 
+      @year = yearNode.content    
+    end
+
+    imgNode = doc.xpath(xPath + '/tr[2]/td/table/td[1]/img')[0]
+    if imgNode
+      @img = imgNode[:src].subLink
+    end
     
     descrNode = doc.xpath(xPath + '/tr[3]/td/pre')[0]
     if descrNode
@@ -79,10 +96,10 @@ def parseSingers
 
     arr = Array.new
     i = 0
-    doc.css('ul#ispol li a').each do |link| 
-        arr.push Singer.new(link[:href].subLink)
-        #break
-        puts i
+    doc.css('ul#ispol li a').each do |link|         
+        singer = Singer.new(link[:href].subLink)
+        arr.push singer
+        puts i.to_s + " " + singer.name
         i += 1        
     end 
     return arr
@@ -133,6 +150,9 @@ def makeXML(singers)
   doc.to_s  
 end
 
+#singer = Singer.new('http://jazzbase.ru/people/3544.htm')
+# singer = Singer.new('http://jazzbase.ru/people/11805.htm')
+# puts singer
 singers = parseSingers
 puts 'Length of singers array: ' + singers.length.to_s
 #puts singers[0].styles
